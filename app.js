729ed56eb9affe32f39f3d7bc5fb3ceeb456ed0a
Aaron-Ale01 <83286987+Aaron-Ale01@users.noreply.github.com>
2022-04-27 16:53:28 -0300

    Primer COMMIT / TP3_PWD

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onload = inicio;
+
+
+function inicio() {
+    document.getElementById('title').style.color = 'aqua';
+    document.getElementById('title').style.backgroundColor = 'brown';
+}
+window.onload = parrafo;
+
+
+var text = document.createElement('h3'); //creo variable y
+text.innerHTML = 'Segundo texto'; //coloco texto
+
+console.log('text') //sale por consola
